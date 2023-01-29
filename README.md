0a36a2545d4a987bea6be32dfb6e4e80219e730c
RamanPan <ramanpan123@gmail.com>
2023-01-29 17:01:29 +0300

    Start of project development

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LearnFastRead(Backend)
- Service for learning speed reading based on the program of V. Wormsbecher and D. Cab with tracking user progress.
+
+Service for learning speed reading based on the program of V. Wormsbecher and D. Cab with tracking user progress.
---
+++ src/main/java/ru/ramanpan/learnfastread/LearnFastReadBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.ramanpan.learnfastread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFastReadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnFastReadBackendApplication.class, args);
+    }
+
+}
