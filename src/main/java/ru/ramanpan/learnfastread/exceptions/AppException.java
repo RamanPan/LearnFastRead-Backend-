b384236f7e01c4ac56c5a46d766bfb0f28d43b35
RamanPan <ramanpan123@gmail.com>
2023-02-08 19:52:55 +0300

    Create exceptions for app

---
+++ src/main/java/ru/ramanpan/learnfastread/exceptions/AppException.java
@@ -0,0 +1,7 @@
+package ru.ramanpan.learnfastread.exceptions;
+
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/ramanpan/learnfastread/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.ramanpan.learnfastread.exceptions;
+
+public class NotFoundException extends AppException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
