a41dc109fa7697de8163a28e2a97da7bc4c8c47f
Roman Panaiot <ramanpan123@gmail.com>
2023-03-12 13:10:52 +0300

    Item service done

--- src/main/java/ru/ramanpan/learnfastread/services/ItemService.java
+++ src/main/java/ru/ramanpan/learnfastread/services/ItemService.java
@@ -1,4 +1,18 @@
 package ru.ramanpan.learnfastread.services;
 
+import ru.ramanpan.learnfastread.dto.ItemDTO;
+import ru.ramanpan.learnfastread.models.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    Long save(ItemDTO itemDTO);
+
+    Long update(ItemDTO itemDTO);
+
+    void delete(Long id);
+
+    List<Item> getAllItems();
+
+    Item getItemById(Long id);
 }
